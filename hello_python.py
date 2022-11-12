412336d16a359eae8d841af1eb24db5f9b8b5eab
arya <aryaytc@gmail.com>
2022-11-12 22:44:10 +0530

    created new file

--- hello_python.py
+++ hello_python.py
@@ -1,4 +1,12 @@
-print("Hi This is Arya")
-a =5
-b =10
+print("Hi,Guys this is ARYA")
+print("A.Arya\tAlagappan \n A210 Shri Harihar Enclave \n Old Natham road \n Madurai 625014")
+a=5
+b=8
 print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(4+9)
+print(5-2)
+print(8*4)
+print(5/5)
