60a4727095d6f7d5bb5c717fa1a0f0eb8f7dc5eb
arya <aryaytc@gmail.com>
2022-11-13 10:09:17 +0530

    created new file

--- arya_finance.py
+++ arya_finance.py
@@ -1,4 +1,5 @@
 import argparse
+import numpy as np
 import pandas as pd
 
 
@@ -6,31 +7,51 @@ import pandas as pd
 class AryaFinance():
     def __init__(self, date, amount, tran_type, comments):
         self.date = date
-        self.amount = amount
-        self.tran_type = tran_type
-        self.comments = comments
+        self.amount = float(amount)
+        self.tran_type = tran_type.upper()
+        self.comments = str(comments)
+        self.df = pd.DataFrame()
 
-    def print_msg(self):
+    def load_transaction(self):
         dict = {'Date': [self.date], 'Amount': [self.amount], 'Tran_Type': [self.tran_type], 'Comments': [comments]}
         input_df = pd.DataFrame.from_dict(dict)
         source_df = pd.read_excel(r'C:\Finance\finance.xlsx')
-        df = pd.concat([source_df, input_df])
-        df.to_excel(r'C:\Finance\finance.xlsx', index=False)
-        print(df)
+        self.df = pd.concat([source_df, input_df])
+        self.df = self.df.reset_index(drop=True)
+        self.calculate_balance()
+        self.df.to_excel(r'C:\Finance\finance.xlsx', index=False)
+
+    def calculate_balance(self):
+        balance = 0
+        self.df['Balance'] = np.NAN
+        for index, row in self.df.iterrows():
+            if row['Tran_Type'] == 'CR':
+                balance = balance + row['Amount']
+            if row['Tran_Type'] == 'DR':
+                balance = balance - row['Amount']
+            self.df.at[index, 'Balance'] = balance
+        print(self.df)
 
 
 
+
+# if __name__ == '__main__':
+#     ap = argparse.ArgumentParser()
+#     ap.add_argument("Date", help="Please pass the date of transaction")
+#     ap.add_argument("Amount", help="Please pass the Amount")
+#     ap.add_argument("Transaction_type", help="Please pass the transaction of type (cr / dr)")
+#     ap.add_argument("Comments", help="Please pass the Comments")
+#     args = ap.parse_args()
+#     date = args.Date
+#     amount = args.Amount
+#     comments = args.Comments
+#     tran_type = args.Transaction_type
+#     AryaFinance(date, amount, tran_type, comments).load_transaction()
 
 
 if __name__ == '__main__':
-    ap = argparse.ArgumentParser()
-    ap.add_argument("Date", help="Please pass the date of transaction")
-    ap.add_argument("Amount", help="Please pass the Amount")
-    ap.add_argument("Transaction_type", help="Please pass the transaction of type (cr / dr)")
-    ap.add_argument("Comments", help="Please pass the Comments")
-    args = ap.parse_args()
-    date = args.Date
-    amount = args.Amount
-    comments = args.Comments
-    tran_type = args.Transaction_type
-    AryaFinance(date, amount, tran_type, comments).print_msg()
+    date = input("Please Enter the Date of transaction ")
+    amount = input("Please Enter the Amount")
+    tran_type = input("Please Enter the Type of transaction(CR or DR) ")
+    comments = input("Please Enter Where You Got Or Spent The Money ")
+    AryaFinance(date, amount, tran_type, comments).load_transaction()
