4c4761453a85dfa863e76ee681807c470380d0bd
arya <aryaytc@gmail.com>
2022-12-02 10:46:00 +0530

    created new file

--- hello_python.py
+++ hello_python.py
@@ -1,5 +1,5 @@
 print("Hi,Guys this is ARYA \n")
-print("\tA.Arya Alagappan, \n A210 Shri Harihar Enclave, \n Old Natham road, \n Madurai 625014.")
+print("\t A.Arya Alagappan,\n\t A210 Shri Harihar Enclave,\n\t Old Natham road,\n\t Madurai 625014.")
 a=25
 b=12
 print(a+b)
