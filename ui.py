510af44f44d3f058d71452710e6cc9b5a1e66614
arya <aryaytc@gmail.com>
2023-02-24 23:21:46 +0530

    created new file

--- ui.py
+++ ui.py
@@ -1,14 +1,19 @@
 import streamlit as st
-
+from datetime import datetime
 # Set page title
 st.set_page_config(page_title="My Streamlit App")
 
 # Add a title to the app
 st.title("Welcome to my Streamlit App")
 
+blood_groups = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']
+
 # Add a text box to the app
-user_input = st.text_input("Enter your name:")
+user_input = st.text_input("Please Enter Your Name:")
 user_input = st.text_input("Please Enter Your Phone Number:")
+dob = st.date_input("Enter your date of birth", datetime.today())
+selected_bg = st.selectbox("Select your blood group", blood_groups)
+
 
 # Add a button to the app
 button_clicked = st.button("Submit")
@@ -17,3 +22,5 @@ button_clicked = st.button("Submit")
 if button_clicked:
     st.write(f"Hello, {user_input}!")
     st.write(f"Phone number is  {user_input}!")
+    st.write(f"Your date of birth is: {dob}")
+    st.write("Your selected blood group is:", selected_bg)
