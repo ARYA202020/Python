95ec58e30fcbd7fba3c7ef69da2a18bf093b22f5
arya <aryaytc@gmail.com>
2023-02-24 23:26:08 +0530

    created new file

--- ui.py
+++ ui.py
@@ -9,10 +9,10 @@ st.title("Welcome to my Streamlit App")
 blood_groups = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']
 
 # Add a text box to the app
-user_input = st.text_input("Please Enter Your Name:")
-user_input = st.text_input("Please Enter Your Phone Number:")
+name = st.text_input("Please Enter Your Name:")
+phone = st.text_input("Please Enter Your Phone Number:")
 dob = st.date_input("Enter your date of birth", datetime.today())
-selected_bg = st.selectbox("Select your blood group", blood_groups)
+b_grp = st.selectbox("Select your blood group", blood_groups)
 
 
 # Add a button to the app
@@ -20,7 +20,14 @@ button_clicked = st.button("Submit")
 
 # Display a message if the button is clicked
 if button_clicked:
-    st.write(f"Hello, {user_input}!")
-    st.write(f"Phone number is  {user_input}!")
-    st.write(f"Your date of birth is: {dob}")
-    st.write("Your selected blood group is:", selected_bg)
+    # st.write(f"Hello, {name}!")
+    # st.write(f"Phone number is  {phone}!")
+    # st.write(f"Your date of birth is: {dob}")
+    # st.write("Your selected blood group is:", b_grp)
+    dict = {
+        'Name': {name},
+        'Phone': {phone},
+        'DOB': {dob},
+        'bld_grp': b_grp
+    }
+    st.write(dict)
