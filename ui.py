e98801530a93944b04d8e4b04123f81cd5a19fbe
arya <aryaytc@gmail.com>
2023-02-24 23:11:07 +0530

    created new file

--- ui.py
+++ ui.py
@@ -8,6 +8,7 @@ st.title("Welcome to my Streamlit App")
 
 # Add a text box to the app
 user_input = st.text_input("Enter your name:")
+user_input = st.text_input("Please Enter Your Phone Number:")
 
 # Add a button to the app
 button_clicked = st.button("Submit")
@@ -15,3 +16,4 @@ button_clicked = st.button("Submit")
 # Display a message if the button is clicked
 if button_clicked:
     st.write(f"Hello, {user_input}!")
+    st.write(f"Phone number is  {user_input}!")
