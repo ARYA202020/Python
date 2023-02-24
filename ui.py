6f7fe05015677aa9812476344468310a1cc8fc18
arya <aryaytc@gmail.com>
2023-02-24 23:38:25 +0530

    created new file

--- ui.py
+++ ui.py
@@ -12,6 +12,10 @@ name = st.text_input("Please Enter Your Name:")
 phone = st.text_input("Please Enter Your Phone Number:")
 dob = st.date_input("Enter your date of birth", datetime.today())
 b_grp = st.selectbox("Select your blood group", blood_groups)
+street = st.text_input("Street")
+city = st.text_input("City")
+state = st.text_input("State")
+pin_code = st.text_input("Pin Code")
 
 
 button_clicked = st.button("Submit")
@@ -22,7 +26,11 @@ if button_clicked:
         'Your Name Is': name,
         'Your Phone Number Is': phone,
         'Your DOB Is': dob,
-        'Your Blood Group Is': b_grp
+        'Your Blood Group Is': b_grp,
+        'Your Street is': street,
+        'Your City is': city,
+        'Your State is': state,
+        'Your Pin Code is': pin_code
     }
     st.write(dict)
 
